ea06f059316e5b90c8bedc5e7ccdf01abd3b32b6
senrajkumar1998 <senrajkumar1998@gmail.com>
2022-12-27 12:51:06 +0530

    first commit

---
+++ src/main/java/com/Accio/SearchResult.java
@@ -0,0 +1,23 @@
+package com.Accio;
+
+public class SearchResult {
+    String pageTitle;
+
+    public String getPageTitle() {
+        return pageTitle;
+    }
+
+    public void setPageTitle(String pageTitle) {
+        this.pageTitle = pageTitle;
+    }
+
+    public String getPageLink() {
+        return pageLink;
+    }
+
+    public void setPageLink(String pageLink) {
+        this.pageLink = pageLink;
+    }
+
+    String pageLink;
+}
